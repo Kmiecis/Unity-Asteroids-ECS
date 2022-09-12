25a63e87421bed028636ddc40b611ed08bc2b029
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-09-12 06:13:52 +0200

    Frames per second

---
+++ Assets/Scripts/Behaviours/FramesPerSecond.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Asteroids
+{
+    public class FramesPerSecond : MonoBehaviour
+    {
+        [SerializeField]
+        protected Text text;
+
+        private int _frames = 0;
+        private float _sum = 0.0f;
+
+        private void Start()
+        {
+            text.text = 0.ToString();
+            text.color = Color.red;
+        }
+
+        private void Update()
+        {
+            var deltaTime = Time.deltaTime;
+
+            _frames++;
+            _sum += deltaTime;
+
+            if (_sum > 0.5f)
+            {
+                var fps = _frames / _sum;
+
+                text.text = fps.ToString("F2");
+                if (fps > 30.0f)
+                {
+                    text.color = Color.green;
+                }
+                else if (fps > 15.0f)
+                {
+                    text.color = Color.yellow;
+                }
+                else
+                {
+                    text.color = Color.red;
+                }
+
+                _frames = 0;
+                _sum = 0.0f;
+            }
+        }
+    }
+}
